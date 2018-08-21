a4d619024c74c357638258892cfeae6db0274698
1160275520 <wangy3@carleton.edu>
2018-08-20 21:21:00 -0500

    add delete function

---
+++ app/Http/Controllers/ServicesController.php
@@ -0,0 +1,124 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use App\Http\Requests;
+use Request;
+use Illuminate\Support\Facades\DB;
+use App\Service;
+class ServicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (session()->has('username')) {
+            $typeList = DB::select("select * from services");
+            return view('services.index',[
+                'typeList'=>$typeList,
+            ]);
+        }else{
+            return view('log',[
+                'faild'=>"请登录！",
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $typeList = DB::select("select * from type_of_service");
+        return view('services.index',[
+                'typeList'=>$typeList,
+            ]);
+        // if (session()->has('username')) {
+        //     $typeList = DB::select("select * from services");
+        //     return view('services.index',[
+        //         'typeList'=>$typeList,
+        //     ]);
+        // }else{
+        //     return view('log',[
+        //         'faild'=>"请登录！",
+        //     ]);
+        // }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        $service = Service::findOrFail($id);
+        return view('services.edit', compact('service'));
+        // if (session()->has('username')) {
+        //     $typeList = DB::select("select * from services");
+        //     return view('services.index',[
+        //         'typeList'=>$typeList,
+        //     ]);
+        // }else{
+        //     return view('log',[
+        //         'faild'=>"请登录！",
+        //     ]);
+        // }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "It is working";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $service = Service::find($id);
+        $service->delete();
+        return redirect('/services');
+        // DB::delete('delete from type_of_service where id = ?', [$id]);
+    }
+}
---
+++ resources/views/services/create.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+    <title>Ample Admin Template - The Ultimate Multipurpose admin template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Footable CSS -->
+    <link href="footable/css/footable.core.css" rel="stylesheet">
+    <link href="bootstrap-select/bootstrap-select.min.css" rel="stylesheet" />
+    <!-- Menu CSS -->
+    <link href="sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+    <!-- animation CSS -->
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="css/colors/blue-dark.css" id="theme" rel="stylesheet">
+</head>
+
+<body class="fix-header">
+    <div id="wrapper">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <nav class="navbar navbar-default navbar-static-top m-b-0">
+            <div class="navbar-header">
+                <div class="top-left-part">
+                    <!-- Logo -->
+                    <a class="logo" href="index.html">
+                        <!-- Logo icon image, you can use font-icon also --><b>
+                        <!--This is dark logo icon--><img src="img/admin-logo.png" alt="home" class="dark-logo" /><!--This is light logo icon--><img src="img/admin-logo-dark.png" alt="home" class="light-logo" />
+                     </b>
+                        <!-- Logo text image you can use text also --><span class="hidden-xs">
+                        <!--This is dark logo text--><img src="img/admin-text.png" alt="home" class="dark-logo" /><!--This is light logo text--><img src="img/admin-text-dark.png" alt="home" class="light-logo" />
+                     </span> </a>
+                </div>
+                <!-- /Logo -->
+                <!-- Search input and Toggle icon -->
+                <ul class="nav navbar-top-links navbar-left">
+                    <li><a href="/listOfServiceType">服务类型</a></li>
+                    <li><a href="/list">所有订单</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Top Navigation -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav slimscrollsidebar">
+                <div class="sidebar-head">
+                    <h3><span class="fa-fw open-close"><i class="ti-menu hidden-xs"></i><i class="ti-close visible-xs"></i></span> <span class="hide-menu">Panopath</span></h3> </div>
+                <ul class="nav" id="side-menu">
+                    <li class="devider"></li>
+                        <ul class="nav nav-second-level">
+                            <li><a class="active" href="/listOfServiceType"><span class="hide-menu">服务类型</span></a></li>
+                            <li ><a  href="/list"><span class="hide-menu">所有订单</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page Content -->
+        <!-- ============================================================== -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="white-box">
+                            <h3 class="box-title">添加新的服务</h3>
+                                <div class="modal-body">
+                                    <form method="post" action="/services/{{$service->id}}" class="form-horizontal form-material">
+                                        <div class="form-group">
+                                            <input type="hidden" class="form-control" value="" >
+                                            <div class="col-md-12 m-b-20">
+                                                <input type="text" name="name" value="{{$service->name}}" class="form-control" placeholder="服务名称"> </div>
+                                            <div class="col-md-12 m-b-20">
+                                                <input type="text" name="money" value="{{$service->money}}" class="form-control" placeholder="服务价格"> </div>
+                                            <div class="col-md-12 m-b-20">
+                                                <input type="text" name="bonus_rate" value="{{$service->bonus_rate}}" class="form-control" placeholder="利润率"> </div>
+                                            <input type="submia" name="submit">
+                                        </div>
+                                    </form>
+                                </div>
+                                <button type="button" class="btn btn-info waves-effect" data-dismiss="modal">添加</button>
+                                <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">返回</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+            <footer class="footer text-center"> 2017 &copy; Ample Admin brought to you by themedesigner.in </footer>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.min.js"></script>
+</body>
+</html>

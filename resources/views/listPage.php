6622fb53df3288f06a7eefcce461f4025181ee5b
1160275520 <wangy3@carleton.edu>
2018-08-21 15:58:19 -0500

    detail page

--- resources/views/listPage.php
+++ resources/views/listPage.php
@@ -34,18 +34,6 @@
 </head>
 
 <body class="fix-header">
-    <!-- ============================================================== -->
-    <!-- Preloader -->
-    <!-- ============================================================== -->
-    <div class="preloader">
-        <svg class="circular" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
-        </svg>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Wrapper -->
-    <!-- ============================================================== -->
-    <div id="wrapper">
         <!-- Topbar header - style you can find in pages.scss -->
         <?php
         $a = view('Header');
@@ -53,6 +41,7 @@
         echo $html
         ?>
         <!-- Page Content -->
+        <!-- ============================================================== -->
         <div id="page-wrapper">
             <div class="container-fluid">
                 <!-- row -->
@@ -66,11 +55,14 @@
                                         <thead>
                                             <tr>
                                                 <th>No</th>
-                                                <th>销售用户名</th>
+                                                <th>Name</th>
                                                 <th>价格</th>
                                                 <th>购买类型</th>
-                                                <th>创建时间</th>
-                                                <th>更改</th>
+                                                <th>createdAt</th>
+                                        <!--        <th>Age</th>
+                                                <th>Joining date</th>
+                                                <th>Salery</th>-->
+                                                <th>Action</th>
                                             </tr>
                                         </thead>
                                         <form id="pro_list_form" class="form-inline form-search" action="/list" pageSize="15">
@@ -91,62 +83,104 @@
                                                 <button class="fcbtn btn btn-success btn-outline btn-1b">查询</button>
                                             </div>
                                         </form>
+                                      <!--  <div class="col-lg-6 col-sm-12">
+                                            <div class="row">
+                                                <form>
+                                                <div class="col-sm-4 text-right m-b-20">
+                                                </div>
+                                                </form>
+                                            </div>
+                                        </div>-->
                                         <tbody>
                                         <?php
                                         //for ($i=0; $i<sizeof($qrcode); $i++) {
                                         foreach($qrcode as $item) {
-                                            echo " <tr><td>$item->sourceId</td><td>";
-                                            if ($item->username!="无对应销售"){
-                                                echo "<a href='/details/$item->username'>$item->username</a>";
-                                            }
-                                            else{
-                                                echo $item->username;
-                                            }
-                                            echo "</td><td> $item->money</td><td>"; 
-                                            if (!isset($item->name)&& $item->username!="无对应销售"){
-                                                echo"未确认";
-                                            }
-                                            else{
-                                            echo $item->name;
-                                            } 
-                                            echo"</td><td> $item->createdAt</td><td>";
-                                            if(!isset($item->type_id) && $item->username!="无对应销售"){
+                                            echo " <tr>
+                                                <td>$item->sourceId</td>
+                                               <td>";
+                                                  if ($item->username!="无对应销售"){
+                                                      echo "<a href='/detail?username=$item->username'>$item->username</a>";
+                                                  }else{
+                                                      echo $item->username;
+                                                  }
+                                            echo "
+                                                </td>
+                                                <td> $item->money</td>
+                                              <td>"; if (!isset($item->name)&& $item->username!="无对应销售"){echo"未确认";}else {echo $item->name;} echo"</td>
+                                         
+                                                <td> $item->createdAt</td><td>
+                                             ";
+                                             if(!isset($item->type_id) && $item->username!="无对应销售"){
                                                  echo" <button type=\"button\" class=\"btn btn-info update\" data-toggle=\"modal\" data-id=\"1\" data-target=\"#add-contact\">更改</button>";
-                                             }
-                                            else if ($item->username=="无对应销售"){
-                                                echo"<h6>无对应销售无法更新订单</h3>";
-                                             }
-                                            echo "  <button type='button' class=\"btn btn-sm btn-icon btn-pure btn-outline delete-row-btn\" data-toggle=\"tooltip\" data-original-title=\"Delete\"><i class=\"ti-close\" aria-hidden=\"true\"></i></button></td></tr>";
-                                            }
+                                                   }
+                                              echo "  <button type='button' class=\"btn btn-sm btn-icon btn-pure btn-outline delete-row-btn\" data-toggle=\"tooltip\" data-original-title=\"Delete\"><i class=\"ti-close\" aria-hidden=\"true\"></i></button>
+                                                </td>
+                                            </tr>";
+                                        }
                                         ?>
                                         </tbody>
                                         <tfoot>
                                             <tr>
+                                                <td colspan="2">
+                                                    <button type="button" class="btn btn-info btn-rounded" data-toggle="modal" data-target="#add-contact">Add New Contact</button>
+                                                </td>
+                                                <div id="add-contact" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                                                <h4 class="modal-title" id="myModalLabel">Add New Contact</h4> </div>
+                                                            <div class="modal-body">
+                                                                <form class="form-horizontal" action="/list" method="post">
+                                                                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                                                                    <input type="hidden" name="username" value="<?php echo $item->username; ?>">
+                                                                    <input type="hidden" name="sourceId" value="<?php echo $item->sourceId; ?>">
+                                                                    <div class="form-group">
+                                                                        <div class="col-md-12 m-b-20">
+                                                                            <h5 class="m-t-30 m-b-10">选择服务类型</h5>
+                                                                            <select class="selectpicker" name="typeId" data-style="form-control">
+                                                                                <?php
+                                                                                  foreach ($servicesTypeList as $item){
+                                                                                       echo "<option value='$item->id'>$item->name</option>";
+                                                                                  }
+                                                                                ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="modal-footer">
+                                                                            <button type="submit" class="btn btn-info">Save</button>
+                                                                            <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cancel</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                        <!-- /.modal-content -->
+                                                    </div>
+                                                    <!-- /.modal-dialog -->
+                                                </div>
                                                 <td colspan="7">
                                                     <div class="text-right">
+                                                     <!--   <ul class="pagination">
+                                                        </ul>-->
                                                         <ul class="pagination">
                                                             <li class="footable-page-arrow"><a data-page="first" href="/list?pageIndex=1">«</a></li>
                                                             <?php
                                                             if($pageindex >1){
                                                                 echo "<li class=\"footable-page-arrow\"><a data-page=\"prev\" href='/list?pageIndex=".($pageindex-1)."'>‹</a></li>";
                                                             }
-
                                                                 for ($i=4;$i>0;$i--){
                                                                         if (($pageindex-$i)<1){
                                                                             continue;
                                                                         }
                                                                     echo "<li class=\"footable-page \"><a data-page=\"0\" href=\"/list?pageIndex=".($pageindex-$i)."\">".($pageindex-$i)."</a></li>";
                                                                 }
-
                                                                   echo "<li class=\"footable-page active\"><a data-page=\"0\" href=\"/list?pageIndex=".($pageindex)."\">".($pageindex)."</a></li>";
-
                                                             for ($i=1;$i<=4;$i++){
                                                                 if (($pageindex+$i)>$pageend){
                                                                      break;
                                                                 }
                                                                 echo "<li class=\"footable-page \"><a data-page=\"0\" href=\"/list?pageIndex=".($pageindex+$i)."\">".($pageindex+$i)."</a></li>";
                                                             }
-
                                                                if($pageindex <$pageend){
                                                                 echo "<li class=\"footable-page-arrow\"><a data-page=\"next\" href='/list?pageIndex=".($pageindex+1)."'>›</a></li>";
                                                                }
@@ -163,6 +197,7 @@
                         </div>
                     </div>
                 </div>
+            </div>
             <!-- /.container-fluid -->
             <footer class="footer text-center"> 2017 &copy; Ample Admin brought to you by themedesigner.in </footer>
         </div>
@@ -283,5 +318,14 @@
             });
             return false;
         });
+    });
+</script>
+<script>
+    $('.update').each(function () {
+        $(this).click(function () {
+            var _id = $(this).data('id');
+            console.log(_id);
+            $("#uid").val(_id)
+        })
     });
 </script>

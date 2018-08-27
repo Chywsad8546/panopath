6a6eaba2d3aa7ebdac3ea2c85e7ab331202f37ed
ChyTesT <18710160187@163.com>
2018-08-27 18:33:25 +0800

    a

--- resources/views/Header.blade.php
+++ resources/views/Header.blade.php
@@ -2,7 +2,7 @@
     <div class="navbar-header">
         <div class="top-left-part">
             <!-- Logo -->
-            <a class="logo" href="index.html">
+            <a class="logo" href="">
                 <!-- Logo icon image, you can use font-icon also --><b>
                 <!--This is dark logo icon--><img src="img/admin-logo.png" alt="home" class="dark-logo" /><!--This is light logo icon--><img src="{{ asset('img/admin-logo-dark.png')}}" alt="home" class="light-logo" />
              </b>
